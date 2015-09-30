14ed66ab419d03cf6655203d296c5828f5fb4acc
Nick Sellen <git@nicksellen.co.uk>
2015-09-30 20:26:32 +0200

    added start of api angular module

---
+++ api.js
@@ -0,0 +1,23 @@
+const apiModule = angular.module('yunityAPI', []);
+
+apiModule.provider('$yunityAPI', [function(){
+  
+  this.$get = $http => {
+
+    return {
+      
+      authenticate(email, password) {
+        console.log('woudl authenticated', email, password, 'using this', $http);
+      }
+
+    };
+
+  };
+
+  return this;
+
+}]);
+
+apiModule.run(() => {
+  
+});
