85e855c8996b84c743fc05b97198f650acfacf48
Michael Dougherty <maackle.d@gmail.com>
2015-10-03 13:56:25 +0200

    adds PendingRequestsProvider - need to remove other code

--- api.js
+++ api.js
@@ -4,28 +4,100 @@ const apiModule = angular.module('yunityAPI', [
     ngCookies,
 ]);
 
+apiModule.provider('PendingRequests', function() {
+
+    let pendingRequests = {};
+
+    let getRequestId = (config) => {
+        return `${ config.method }:${ config.url }`;
+    }
+
+    let isPending = () => {
+        return Object.keys(pendingRequests).length > 0;
+    }
+
+    return {
+        $get () {
+            return {
+                isPending: isPending
+            }
+        },
+        registerRequest (config) {
+            let id = getRequestId(config);
+            pendingRequests[id] = pendingRequests[id] ? pendingRequests[id] + 1 : 1;
+        },
+        registerResponse (response) {
+            let id = getRequestId(response.config);
+            pendingRequests[id] -= 1;
+            if (pendingRequests[id] == 0) {
+                delete pendingRequests[id];
+            }
+        },
+        isPending: isPending,
+    }
+});
+
+apiModule.config(($httpProvider, PendingRequestsProvider) => {
+
+    let requestTracker = ($q) => {
+
+        let pendingRequests = PendingRequestsProvider.pendingRequests;
+
+        return {
+            request: (config) => {
+                PendingRequestsProvider.registerRequest(config);
+                return config;
+            },
+            response: (response) => {
+                PendingRequestsProvider.registerResponse(response);
+                return response;
+            },
+            responseError: (rejection) => {
+                // TODO: handle errors
+                console.error('caught error: ', rejection);
+                return $q.reject(rejection);
+            }
+        }
+    }
+    $httpProvider.interceptors.push(requestTracker);
+})
+
 apiModule.run(function ($http, $cookies) {
     $http.defaults.headers.common['X-CSRFToken'] = $cookies.get('csrftoken');
 });
 
-apiModule.provider('$yunityAPI', [function () {
+apiModule.factory('$yunityAPI', ['$http', function ($http) {
 
-    this.$get = $http => {
         return {
             url: '/api',
             urlSuffix: '',
+            requestStart: function() {},
+            requestComplete: function() {},
+            requestFailed: function () {},
 
             /*
              * Configuration
              */
             config(opt) {
 
-                if (opt.url != undefined) {
+                if (opt.url !== undefined) {
                     this.url = opt.url;
                 }
 
-                if(opt.urlSuffix != undefined) {
+                if(opt.urlSuffix !== undefined) {
                     this.urlSuffix = opt.urlSuffix;
+                }
+
+                if(opt.requestStart !== undefined) {
+                    this.requestStart = opt.requestStart;
+                }
+
+                if(opt.requestComplete !== undefined) {
+                    this.requestComplete = opt.requestComplete;
+                }
+
+                if(opt.requestFailed !== undefined) {
+                    this.requestFailed = opt.requestFailed;
                 }
 
             },
@@ -36,23 +108,20 @@ apiModule.provider('$yunityAPI', [function () {
              */
             listMappable(opt) {
 
-                this.apiCall({
-                    uri: '/listitems',
-                    method: 'GET',
-                    success: function(ret) {
+                return this.apiCall('/listitems').then(
+                    function(ret) {
                         console.log('listmappables success');
                         if(opt.success != undefined) {
                             opt.success(ret);
                         }
                     },
-                    error: function(ret) {
+                    function(ret) {
                         console.log('listmappables error');
                         if(opt.error != undefined) {
                             opt.error(ret);
                         }
                     }
-                });
-
+                );
             },
 
             /*
@@ -61,26 +130,27 @@ apiModule.provider('$yunityAPI', [function () {
              */
             authenticate(opt) {
 
-                this.apiCall({
-                    uri: '/login/',
-                    method: 'POST',
+                return this.apiCall({
+                    uri: '/login',
+                    method: 'GET',
                     data: {
                         email: opt.email,
                         password: opt.password
                     },
-                    success: function (ret) {
+                }).then(
+                    function (ret) {
                         console.log('auth success');
                         if(opt.success != undefined) {
                             opt.success(ret);
                         }
                     },
-                    error: function (ret) {
+                    function (ret) {
                         console.log('auth error');
                         if(opt.error != undefined) {
                             opt.error(ret);
                         }
                     }
-                });
+                );
 
             },
 
@@ -92,6 +162,21 @@ apiModule.provider('$yunityAPI', [function () {
             apiCall(opt) {
 
                 /*
+                 * make this accessable
+                 */
+                let api = this;
+
+                /*
+                 * If opt is a string, use default values
+                 */
+                if (typeof(opt) === 'string') {
+                    opt = {
+                        url: opt,
+                        method: 'GET'
+                    };
+                }
+
+                /*
                  * if no data and no method specified do GET othwist POST
                  */
                 if (opt.method == undefined && opt.data == undefined) {
@@ -101,11 +186,6 @@ apiModule.provider('$yunityAPI', [function () {
                     opt.method = 'POST';
                 }
 
-                /*
-                 * make this accessable
-                 */
-                let api = this;
-
                 if (opt.uri != undefined) {
                     api.url += opt.uri;
                 }
@@ -114,32 +194,40 @@ apiModule.provider('$yunityAPI', [function () {
                     opt.data = {};
                 }
 
+                api.requestStart();
+
                 /*
                  * RUN Angulars HTTP Call
                  */
-                $http({
+                return $http({
                     method: opt.method,
                     url: api.url + api.urlSuffix,
                     data: opt.data
-                }).then(function successCallback(response) {
-
-                    if (opt.success != undefined) {
-                        opt.success(response.data);
-                    }
-
-                }, function errorCallback(response) {
-
-                    if (opt.error != undefined) {
-                        opt.error(response);
-                    }
-
+                }).then(function (data) {
+                    api.requestComplete();
+                    return data;
                 });
             }
         };
+}]);
+
+apiModule.factory('MapItemService', ['$q', '$yunityAPI', function($q, $yunityAPI) {
+    return {
+        getMapItemsAndUsers: function(opts) {
+            return $q.all([
+                $yunityAPI.apiCall('/items'),
+                $yunityAPI.apiCall('/users'),
+            ], function(items, users) {
+                return {
+                    items: items,
+                    users: users
+                }
+            })
+        },
+        getMapItems: function(opts) {
+            return $yunityAPI.apiCall('/items');
+        }
     };
-
-    return this;
-
 }]);
 
 apiModule.run(() => {
