098a3cec11813e0f9aaf6ddf2347f0ed92d24341
Flo <goessek@posteo.de>
2015-10-03 12:12:06 +0200

    Put translate module into common

---
+++ translate.js
@@ -0,0 +1,29 @@
+const translateModule = angular.module('yunityTranslate', ['pascalprecht.translate']);
+
+translateModule.config(function ($translateProvider) {
+
+  $translateProvider.useStaticFilesLoader({
+    prefix: 'lang-',
+    suffix: '.json'
+  });
+
+	$translateProvider.determinePreferredLanguage();
+  $translateProvider.useSanitizeValueStrategy('sanitize'); // http://angular-translate.github.io/docs/#/guide/19_security
+});
+
+translateModule.controller('LanguageCtrl', function ($scope, $translate) {
+
+  $scope.changeLang = function (key) {
+    $translate.use(key).then(function (key) {
+      console.log("Switched language to " + key + " .");
+    }, function (key) {
+      console.log("Something went wrong!");
+    });
+  };
+});
+
+translateModule.run(() => {
+
+});
+
+export default 'yunityTranslate';
