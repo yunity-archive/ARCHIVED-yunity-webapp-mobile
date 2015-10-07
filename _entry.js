2d54711f82e6ff957365eab81f33eb28b1498128
Raphael Wintrich <raphi@waldorfweb.net>
2015-10-07 17:33:34 +0200

    chat api caals added

--- _entry.js
+++ _entry.js
@@ -4,3 +4,5 @@ import './js/app.js';
 import yunityWall from './components/wall/wall.js';
 
 import yunityChat from './components/chat/chat.js';
+
+import yunitySignup from './components/signup/signup.js';
--- components/chat/chat.js
+++ components/chat/chat.js
@@ -1,17 +1,17 @@
 
-angular.module('yunity.mobile').directive('yCoversationList', function() {
+angular.module('yunity.mobile').directive('yChatList', function() {
     return {
         scope: {},
         restrict: 'E',
         //template: '<h2>Hello Universe!</h2>',
-        templateUrl: 'components/chat/conversation-list.html',
+        templateUrl: 'components/chat/chat-list.html',
         controller: function ($scope, yAPI) {
             $scope.conversations = [];
 
             /*
              * Initial API Call to gel list of conversations
              */
-            yAPI.apiCall('/listconversations').then(function(ret){
+            yAPI.apiCall('/chats').then(function(ret){
 
                // console.log(ret);
 
@@ -21,20 +21,89 @@ angular.module('yunity.mobile').directive('yCoversationList', function() {
     }
 });
 
-angular.module('yunity.mobile').directive('yConversation', function() {
+angular.module('yunity.mobile').directive('yChat', function() {
     return {
         scope: {},
         restrict: 'E',
-        //template: '<h2>Hello Universe!</h2>',
-        templateUrl: 'components/chat/conversation.html',
+        templateUrl: 'components/chat/chat.html',
         controller: function ($scope, yAPI) {
-            $scope.conv = [];
 
             /*
-             * Initial API Call to gel list of messages..
+             * init Conversation List
              */
-            yAPI.apiCall('/getconversation').then(function(ret){
-                $scope.conv = ret.data;
+            $scope.chat = {
+                id:12,
+                name:"Uwe",
+                last_activity:"before 1 minute",
+                messages:[]
+            };
+
+
+            /**
+             * send chat TEXT message function
+             */
+            $scope.sendmessage = function() {
+                if($scope.inputtext !== undefined) {
+                    yAPI.apiCall({
+                        uri: '/chats/' + $scope.chat.id + '/messages',
+                        method: 'POST',
+                        data: {
+                            chatid: $scope.chat.id,
+                            body: {
+                                type: 'TEXT',
+                                content: $scope.inputtext
+                            }
+                        }
+                    }).then(function(ret) {
+                        console.log('chat send success');
+                        console.log(ret);
+                    }, function(ret) {
+                        console.log('chat send failed');
+                        console.log(ret);
+                    });
+                }
+            };
+
+            /**
+             * TO DO: returning correct path to avatar image
+             */
+            $scope.avatar = function(user_id) {
+                return '/img/avatar.png';
+            };
+
+            /**
+             * TO DO: returns user object and handle users to store them in memory
+             *
+             * @param id
+             */
+            $scope.getUser = function(id) {
+
+                /*
+                 so like
+
+                 if(this.users[id] != undefined) {
+                 return this.users[id]
+                 } else {
+                 yAPI.apiCall('/user/' + id).then(function(){
+                 ...
+                 });
+                 }
+
+                 */
+
+                return {
+                    id: 12,
+                    name: "Uwe"
+
+                };
+            };
+
+
+            /*
+             * Initial API Call to get list of messages..
+             */
+            yAPI.apiCall('/chats/' + $scope.chat.id + '/messages').then(function(ret){
+                $scope.chat.messages = ret.data.messages;
             });
         }
     }
