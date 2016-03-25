ea0c146ad9cc9fd05c3b2edb9ba42244e152b712
Nick Sellen <git@nicksellen.co.uk>
2016-03-25 18:27:35 +0100

    Move routes to seperate file

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ import yunityAPI from '../common/api';
 import yunityChat from '../common/chat';
 import yunityMap from '../common/map';
 
+import routes from './routes';
+
 /*
 * INIT APP
 */
@@ -78,109 +80,7 @@ app.run(['$transform', '$rootScope', 'yAPI', '$location', '$route', ($transform,
 
 }]);
 
-/*
-* ROUTUNG
-*/
-app.config(($routeProvider) =>  {
-  $routeProvider.when('/', {
-    template: '<wall-page />',
-    reloadOnSearch: false
-  });
-
-  $routeProvider.when('/signup', {
-    template: '<signup-page />',
-    reloadOnSearch: false
-  });
-
-  $routeProvider.when('/login', {
-    template: '<login-page />',
-    reloadOnSearch: false
-  });
-
-  $routeProvider.when('/groups', {
-    template: '<groups />',
-    reloadOnSearch: false
-  });
-
-  $routeProvider.when('/group/:id', {
-
-    template: (params) =>  {
-      return `<group-page groupid="${params.id}" />`;
-    },
-    reloadOnSearch: false
-  });
-
-  $routeProvider.when('/groups/add', {
-    template: '<groups-add />',
-    reloadOnSearch: false
-  });
-
-  $routeProvider.when('/profile/:id', {
-
-    template: (params) =>  {
-      return `<profile-page userid="${params.id}" />`;
-    },
-    reloadOnSearch: false
-  });
-
-  $routeProvider.when('/chat/:id', {
-    templateUrl: 'chat.html',
-    reloadOnSearch: false,
-    access: {
-      requiresLogin: true
-    }
-  });
-
-  $routeProvider.when('/chat/new/:userids', {
-    template: (params) =>  {
-      return `<chat-page userids="${params.userids}" />`;
-    },
-    reloadOnSearch: false,
-    access: {
-      requiresLogin: true
-    }
-  });
-
-
-  $routeProvider.when('/about', {
-    templateUrl: 'about.html',
-    reloadOnSearch: false
-  });
-
-  $routeProvider.when('/map', {
-    template: '<map-page />',
-    reloadOnSearch: false
-  });
-
-  $routeProvider.when('/create/item', {
-    template: '<create-item-page />',
-    reloadOnSearch: false,
-    access: {
-      requiresLogin: true
-    }
-  });
-
-  $routeProvider.when('/list/items', {
-    template: '<list-items-page />',
-    reloadOnSearch: false
-
-  });
-
-  $routeProvider.when('/item/:id', {
-    template: '<item-detail-page />',
-    reloadOnSearch: false
-
-  });
-
-  $routeProvider.when('/list/users', {
-    template: '<list-users-page />',
-    reloadOnSearch: false,
-    access: {
-      requiresLogin: true
-    }
-  });
-
-});
+app.config(routes);
 
 // app.provider('authFilter', function() {
 //         this.$get = function(yAPI, $location) {
