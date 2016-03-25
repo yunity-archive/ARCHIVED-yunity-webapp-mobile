65dd926a58f73735abddb2fa4388398bb08ca0ea
Nick Sellen <git@nicksellen.co.uk>
2016-03-25 21:46:37 +0100

    Signup page to use controllerAs syntax

--- components/signup/signup-page.js
+++ components/signup/signup-page.js
@@ -3,47 +3,44 @@ import signupTemplate from './signup.html';
 let debug = require('debug')('yunity:signup-page');
 
 export default function() {
-
   return {
     scope: {},
     restrict: 'E',
     templateUrl: signupTemplate,
-    controller: ($scope, yAPI, yChat, $location) => {
-
-      $scope.signup = () => {
-
-        yAPI.apiCall({
-          uri: '/users',
-          method: 'POST',
-          data: {
-            email: $scope.email,
-            password: $scope.password,
-            display_name: $scope.displayname,
-            first_name: $scope.firstname,
-            last_name: $scope.lastname
-          }
-        }).then((res) => {
-
-          //success('signup successfull :)');
-          //alert(res.data.id);
-          yAPI.authenticate({
-            email: $scope.email,
-            password: $scope.password,
-            success: () => {
-              debug('login success');
-              $location.path('/profile/' + res.data.id);
-              yChat.initChats();
-            },
-            error: () => {
-              alert('login failed');
-            }
-          });
-
-        }, () => {
-          alert('error while signup');
-        });
-      };
-
-    }
+    controller: SignupPageCtrl,
+    controllerAs: 'ctrl'
   };
+}
+
+class SignupPageCtrl {
+
+  constructor($scope, yAPI, yChat, $location) {
+    Object.assign(this, { $scope, yAPI, yChat, $location });
+    this.data = {};
+  }
+
+  signup() {
+    this.yAPI.apiCall({
+      uri: '/users',
+      method: 'POST',
+      data: this.data
+    }).then((res) => {
+      let { email, password } = this.data;
+      this.yAPI.authenticate({
+        email, password,
+        success: () => {
+          debug('login success');
+          this.$location.path('/profile/' + res.data.id);
+          this.yChat.initChats();
+        },
+        error: () => {
+          alert('login failed');
+        }
+      });
+
+    }, () => {
+      alert('error while signup');
+    });
+  }
+
 }
