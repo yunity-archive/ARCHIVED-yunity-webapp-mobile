4a3d5074f65012725a7c36478e8b3ba9e45b516c
Nick Sellen <git@nicksellen.co.uk>
2016-03-26 01:19:55 +0100

    Refactored signupPageCtrl into it's own file

--- src/components/signup/signupPageDirective.js
+++ src/components/signup/signupPageDirective.js
@@ -1,47 +1,13 @@
 import signupTemplate from './signupPage.html';
 
-const debug = require('debug')('yunity:signupPage');
+import signupPageCtrl from './signupPageCtrl';
 
 export default function() {
   return {
     scope: {},
     restrict: 'E',
     templateUrl: signupTemplate,
-    controller: SignupPageCtrl,
+    controller: signupPageCtrl,
     controllerAs: 'ctrl'
   };
-}
-
-class SignupPageCtrl {
-
-  constructor($scope, yAPI, yChat, $location) {
-    'ngInject';
-    Object.assign(this, { $scope, yAPI, yChat, $location });
-    this.data = {};
-  }
-
-  signup() {
-    this.yAPI.apiCall({
-      uri: '/users',
-      method: 'POST',
-      data: this.data
-    }).then((res) => {
-      let { email, password } = this.data;
-      this.yAPI.authenticate({
-        email, password,
-        success: () => {
-          debug('login success');
-          this.$location.path('/profile/' + res.data.id);
-          this.yChat.initChats();
-        },
-        error: err => {
-          debug('login failed', err);
-        }
-      });
-
-    }, err => {
-      debug('error while signup', err);
-    });
-  }
-
 }
