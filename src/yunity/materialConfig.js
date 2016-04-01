ac437134917cb25c3acbad011ea0d447a4b9d193
copygirl <halfnerd.copygirl@gmail.com>
2016-04-01 22:56:50 +0200

    Change some additional palette colors

--- src/yunity/materialConfig.js
+++ src/yunity/materialConfig.js
@@ -2,9 +2,13 @@ export default function materialConfig($mdThemingProvider) {
   'ngInject';
   $mdThemingProvider.theme('default')
     .primaryPalette('pink', {
-      'default': '600'
+      'default': '600',
+      'hue-1': '400'
     })
     .accentPalette('blue', {
       'default': '800'
+    })
+    .backgroundPalette('grey', {
+      'default': '200'
     });
 }
