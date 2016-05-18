4e495c1e687e30d4eaf34e5aa230bac24ffc3bf5
copygirl <halfnerd.copygirl@gmail.com>
2016-05-18 20:06:19 +0200

    Add (unfinished) "yunityBlue" palette

--- src/yunity/materialConfig.js
+++ src/yunity/materialConfig.js
@@ -1,12 +1,34 @@
 export default function materialConfig($mdThemingProvider) {
   'ngInject';
+  
+  $mdThemingProvider.definePalette('yunityBlue', {
+    '50': 'FFFFFF',
+    '100': 'FFFFFF',
+    '200': 'FFFFFF',
+    '300': 'FFFFFF',
+    '400': 'FFFFFF',
+    '500': '196687',
+    '600': 'FFFFFF',
+    '700': '124C65',
+    '800': 'FFFFFF',
+    '900': 'FFFFFF',
+    'A100': 'FFFFFF',
+    'A200': 'FFFFFF',
+    'A400': 'FFFFFF',
+    'A700': 'FFFFFF',
+    'contrastDefaultColor': 'light',
+    'contrastDarkColors': [ '50', '100', '200',
+                            '300', '400', 'A100' ],
+  });
+  
   $mdThemingProvider.theme('default')
     .primaryPalette('pink', {
       'default': '600',
       'hue-1': '400'
     })
-    .accentPalette('blue', {
-      'default': '800'
+    .accentPalette('yunityBlue', {
+      'default': '500',
+      'hue-1': '700'
     })
     .backgroundPalette('grey', {
       'default': '200',
